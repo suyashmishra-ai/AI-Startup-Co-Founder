c6669c423e302b90f071da8a403b8559525b927e
Suyash Mishra <suyashmishra1014@gmail.com>
2026-07-24 04:45:58 +0530

    Clean up Vercel configs for standard Next.js deployment

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add root directory to python path for imports
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from backend.main import app
